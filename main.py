db0d110c8ca41bb3c7d363cccfccb7eb4248159d
KohamaKaito <e195746@ie.u-ryukyu.ac.jp>
2021-07-21 18:31:44 +0900

    ラベルエンコーダー

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import pandas as pd 
+from sklearn.preprocessing import LabelEncoder
 
 
-# import data
+# データセットを読み込む
 train = pd.read_csv('input/train.csv')
 test = pd.read_csv('input/test.csv')
 
+"""
 print(train.dtypes)
+print(train['MSZoning'].unique())
+"""
+
+# ラベルエンコーダー
+for i in range(train.shape[1]):
+    if train.iloc[:,i].dtypes == object:
+        lbl = LabelEncoder()
+        lbl.fit(list(train.iloc[:,i].values) + list(test.iloc[:,i].values))
+        train.iloc[:,i] = lbl.transform(list(train.iloc[:,i].values))
+        test.iloc[:,i] = lbl.transform(list(test.iloc[:,i].values))
+
+"""
+print(train.dtypes)
+print(train['MSZoning'].unique())
+"""
